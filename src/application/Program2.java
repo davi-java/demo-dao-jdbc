6292f3c96cbd3a7bd60d57836f869db8e1fa9ed5
davi-java <david96711972@gmail.com>
2020-05-31 08:13:54 -0300

    Department Update

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -11,10 +11,14 @@ public class Program2 {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("Teste 1 Department Insert");
-		Department department = new Department(null, "Musica");
+		Department department = new Department(null, "Cosmetico");
 		departmentDao.insert(department);
 		System.out.println("Id new Insert: "+department.getId());
 
+		System.out.println("Teste 2 Department Update");
+		department.setName("New Cosmetico!");
+		departmentDao.update(department);
+		System.out.println("Update Completed!");
 	}
 
 }
